a4de8f836c2105aec677c95448f3d9f277f4efb5
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-01 21:45:39 +0200

    Uploaded Latest

--- src/main/kotlin/de/jakkoble/JKKBot.kt
+++ src/main/kotlin/de/jakkoble/JKKBot.kt
@@ -4,29 +4,28 @@ import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import discord4j.core.`object`.entity.Message
 import reactor.core.publisher.Mono
 
 
 object JKKBot {
     @JvmStatic
     fun main(args: Array<String>) {
-        val client = DiscordClient.create("MTAwMzM4MjM2NDg0OTg0ODM2MA.GPahhC._pdE8dfgWEzkEs_bHmBYCQyBqBb98XRv-0ewFg")
-        val login = client.withGateway { gateway: GatewayDiscordClient ->
-            val printOnLogin = gateway.on(ReadyEvent::class.java) { event: ReadyEvent ->
-                Mono.fromRunnable<Any?> {
-                    val self = event.self
-                    println("Logged in as ${self.username}#${self.discriminator}")
-                }
-            }.then()
-            val handlePingCommand = gateway.on(MessageCreateEvent::class.java) { event: MessageCreateEvent ->
-                val message: Message = event.message
-                if (message.content.equals("!ping", true))
-                    return@on message.channel.flatMap { channel -> channel.createMessage("pong!") }
-                Mono.empty<Any?>()
-            }.then()
-            printOnLogin.and(handlePingCommand)
-        }
+        val login = DiscordClient.create(System.getenv("TOKEN")).withGateway { gateway: GatewayDiscordClient ->
+                // ReadyEvent example
+                val printOnLogin = gateway.on(ReadyEvent::class.java) { event: ReadyEvent ->
+                    Mono.fromRunnable<Any?> { println("Logged in as ${event.self.username}#${event.self.discriminator}...") }
+                }.then()
+
+                // MessageCreateEvent example
+                val handlePingCommand = gateway.on(MessageCreateEvent::class.java) { event: MessageCreateEvent ->
+                    if (event.message.content.equals("!ping", true)) {
+                        println("Responded with Message 'pong!'.")
+                        return@on event.message.channel.flatMap { channel -> channel.createMessage("pong!") }
+                    }
+                    Mono.empty<Any?>()
+                }.then()
+                printOnLogin.and(handlePingCommand)
+            }
         login.block()
     }
 }
