8edfd39cb6fa9c46a6bb1c0baa47e3e9d43fc2c4
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-02 19:16:06 +0200

    Uploaded Latest

--- src/main/kotlin/de/jakkoble/JKKBot.kt
+++ src/main/kotlin/de/jakkoble/JKKBot.kt
@@ -2,21 +2,37 @@ package de.jakkoble
 
 import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
-import discord4j.core.event.ReactiveEventAdapter
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import org.reactivestreams.Publisher
+import discord4j.core.`object`.entity.Message
+import discord4j.core.`object`.entity.User
 import reactor.core.publisher.Mono
 
 
 object JKKBot {
     @JvmStatic
     fun main(args: Array<String>) {
-        val client = DiscordClient.create("MTAwMzM4MjM2NDg0OTg0ODM2MA.GPahhC._pdE8dfgWEzkEs_bHmBYCQyBqBb98XRv-0ewFg")
-        GlobalCommandRegistrar(client).registerCommands(listOf("greet.json", "ping.json"))
+        val discordClient = DiscordClient.create("MTAwMzM4MjM2NDg0OTg0ODM2MA.GPahhC._pdE8dfgWEzkEs_bHmBYCQyBqBb98XRv-0ewFg") ?: return
+        val login: Mono<Void> = discordClient.withGateway { gateway: GatewayDiscordClient ->
+            val readyEventFlux = gateway.on(ReadyEvent::class.java) { event: ReadyEvent ->
+                Mono.fromRunnable<Any?> {
+                    val self: User = event.self
+                    System.out.printf("Logged in as %s#%s%n", self.username, self.discriminator)
+                }
+            }.then()
+            val messageCreateFlux = gateway.on(MessageCreateEvent::class.java) { event: MessageCreateEvent ->
+                val message: Message = event.message
+                if (message.content.equals("!ping", true)) {
+                    return@on message.channel.flatMap { channel -> channel.createMessage("pong!") }
+                }
+                return@on Mono.empty()
+            }
+            readyEventFlux.and(messageCreateFlux)
+        }
+        login.block()
+        //GlobalCommandRegistrar(client).registerCommands(listOf("greet.json", "ping.json"))
 
-        val login = client.withGateway { gateway: GatewayDiscordClient ->
+        /*val login = client.withGateway { gateway: GatewayDiscordClient ->
             // ReadyEvent example
             val printOnLogin = gateway.on(ReadyEvent::class.java) { event: ReadyEvent ->
                 Mono.fromRunnable<Any?> { println("Logged in as ${event.self.username}#${event.self.discriminator}...") }
@@ -43,6 +59,6 @@ object JKKBot {
             })?.then()
             printOnLogin.and(handlePingCommand).and(handleCommand)
         }
-        login.block()
+        login.block()*/
     }
 }
