e85d04d8043ba3888cf1f424da4bdcc0eb6fa5ab
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-03 22:32:22 +0200

    Added Comments

--- src/main/kotlin/de/jakkoble/JKKBot.kt
+++ src/main/kotlin/de/jakkoble/JKKBot.kt
@@ -13,32 +13,46 @@ import discord4j.core.spec.InteractionApplicationCommandCallbackSpec
 import discord4j.rest.util.Color
 import kotlinx.coroutines.runBlocking
 import reactor.core.publisher.Mono
+import kotlin.system.exitProcess
 
 
 object JKKBot {
     private lateinit var discordClient: DiscordClient
 
+    // Change for your Situation
+    private val botToken: String = System.getenv("TOKEN") ?: exitProcess(1) // Do not write your Bot Token in your Source Code => Save it in an Environmental Variable
+    private const val guildId = 1003362781791256596 // ServerID of the Bot
+    private const val channelId = 1003392961029087323 // Channel of Status Message
+
     @JvmStatic
     fun main(args: Array<String>) {
-        discordClient = DiscordClient.create("MTAwMzM4MjM2NDg0OTg0ODM2MA.GPahhC._pdE8dfgWEzkEs_bHmBYCQyBqBb98XRv-0ewFg")
-        GlobalCommandRegistrar(discordClient).registerCommands(listOf("ping.json", "user.json"))
+        discordClient = DiscordClient.create(botToken) // Create a new Discord Client with Token => "Bot is still "logged out"
+        GlobalCommandRegistrar(discordClient).registerCommands(listOf("ping.json", "user.json")) // Register Commands from json Files
 
-        val login: Mono<Void> = discordClient.withGateway { gateway: GatewayDiscordClient ->
+        val login: Mono<Void> = discordClient.withGateway { gateway: GatewayDiscordClient -> // Open up a new GatewayDiscordClient => Bot "logged in"
+
+            // Listen to the ReadyEvent => Bot is launched and ready
             val readyHandler = gateway.on(ReadyEvent::class.java) { event: ReadyEvent ->
                 Mono.fromRunnable<Any?> {
                     val self: User = event.self
                     println("Logged in as ${self.username}#${self.discriminator}")
+
+                    // Create a new Embed with some Specs
                     val embed: EmbedCreateSpec = EmbedCreateSpec.builder().color(Color.GREEN).title("Status: Online")
                         .description("JKKBot successfully started and is now online.")
                         .thumbnail("https://i.imgur.com/FMiS7Xg.jpg")
                         .build()
+
+                    // Run in new Coroutine to not interrupt the flow
                     runBlocking {
-                        discordClient.getChannelById(Snowflake.of(1003392961029087323))
+                        discordClient.getChannelById(Snowflake.of(channelId))
                             .createMessage(embed.asRequest())
                             .subscribe()
                     }
                 }
             }.then()
+
+            // Listen to the ChatInputInteractionEvent => on Slash Command
             val commandHandler = gateway.on(ChatInputInteractionEvent::class.java) { event: ChatInputInteractionEvent ->
                 if (event.commandName.equals("ping")) return@on event.reply("pong!")
                 else if (event.commandName.equals("user")) {
@@ -46,29 +60,32 @@ object JKKBot {
                         .flatMap { obj: ApplicationCommandInteractionOption -> obj.value }
                         .map { obj: ApplicationCommandInteractionOptionValue -> obj.raw }
                         .get()
+
+                    // Reply with the created User Embed of getUserEmbed() Methode (Bottom of this class)
                     return@on event.reply(InteractionApplicationCommandCallbackSpec.builder()
                         .addEmbed(getUserEmbed(userId.toLong()))
                         .build())
                 }
-                return@on Mono.empty()
+                return@on Mono.empty() // If nothing replied yet, an empty Mono replies => "nothing" happens
             }
+            // Combine the two Event Listeners (not necessary for just one Event)
             readyHandler.and(commandHandler)
         }
         login.block()
     }
-
     private fun getUserEmbed(userId: Long): EmbedCreateSpec {
-        val memberData = discordClient.getMemberById(Snowflake.of(1003362781791256596), Snowflake.of(userId)).data.block() ?: return getErrorEmbet()
-        val userData = memberData.user() ?: return getErrorEmbet()
+        val memberData = discordClient.getMemberById(Snowflake.of(guildId), Snowflake.of(userId)).data.block() ?: return getErrorEmbed()
+        val userData = memberData.user() ?: return getErrorEmbed()
         return EmbedCreateSpec.builder().color(Color.PINK).title("User Data Request")
             .description("These are some Information of the given User <@$userId>")
             .thumbnail("https://cdn.discordapp.com/avatars/$userId/${userData.avatar().get()}?size=64")
             .addField("Name", userData.username(), true)
             .addField("Tag", userData.discriminator(), true)
             .addField("ID", userId.toString(), true)
-            .build() ?: return getErrorEmbet()
+            .build() ?: return getErrorEmbed()
     }
-    private fun getErrorEmbet(): EmbedCreateSpec = EmbedCreateSpec.builder().color(Color.RED).title("Error")
+    // Gets returned in different places when something went wrong (Embed)
+    private fun getErrorEmbed(): EmbedCreateSpec = EmbedCreateSpec.builder().color(Color.RED).title("Error")
         .description("Something went wrong. Please contact the Server Team.")
         .build()
 }
